f8048de5ff755052fe209613fba654e6db054759
MinHK4 <67371123+MinHK4@users.noreply.github.com>
2022-03-31 08:51:27 +0900

    Time: 4 ms (77.68%), Space: 9.6 MB (51.96%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -6,6 +6,9 @@ public:
         int rows = matrix.size();
         int cols = matrix[0].size();
         
+        // 이분탐색을 이용해서 어떤 행에서 봐야할지 먼저 찾고
+        // 각 열의 시작점을 잡아서 그것보다는 크다는 전제를 바탕으로 했는데
+        // 아예 타겟이 그것보다 작은 경우가 있을 수 있어서 t_row 초기화 0으로 해주기!!
         int lo = 0;
         int hi = rows-1;
         int t_row = 0;
@@ -19,6 +22,7 @@ public:
                 hi = mid - 1;
         }
         
+        // 이분 탐색을 이용해서 해당 행에서 target이 있는지 열을 훑어보기
         lo = 0;
         hi = cols-1;
         while(lo <= hi){
