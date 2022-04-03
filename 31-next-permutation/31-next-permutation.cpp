2688c4e20ae250e56546da16f3c7343155740d91
MinHK4 <67371123+MinHK4@users.noreply.github.com>
2022-04-03 15:39:05 +0900

    Time: 12 ms (13.14%), Space: 12.1 MB (37.25%) - LeetHub

---
+++ 31-next-permutation/31-next-permutation.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        // STL에 있는 next_permutation 함수 쓰면 한 줄에 해결 가능하긴 함
+        // next_permutation(nums.begin(), nums.end());
+        
+        // 1.
+        int size = nums.size(), k, l;
+        for(k=size-2; k>=0; k--){
+            if(nums[k] < nums[k+1])
+                break;
+        }
+        
+        // 1-1)
+        if(k<0)
+            reverse(nums.begin(), nums.end());
+        // 2.
+        else{
+            for(l=size-1; l>k; l--){
+                if(nums[k] < nums[l])
+                    break;
+            }
+            // 3. & 4.
+            swap(nums[k], nums[l]);
+            reverse(nums.begin()+k+1, nums.end());
+        }
+    }
+};
+
+/*
+1. Find the largest index k such that nums[k] < nums[k + 1]. 
+1-1) If no such index exists, just reverse nums and done.
+2. Find the largest index l > k such that nums[k] < nums[l].
+3. Swap nums[k] and nums[l].
+4. Reverse the sub-array nums[k + 1:].
+*/
