311520ed364e4a4afde7988035186408ccb6148f
MinHK4 <67371123+MinHK4@users.noreply.github.com>
2022-04-16 22:29:47 +0900

    Time: 40 ms (81.14%), Space: 33.5 MB (19.45%) - LeetHub

--- 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.cpp
+++ 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.cpp
@@ -10,7 +10,14 @@
  * };
  */
 class Solution {
+
+private:
+    int cur_sum = 0;
+    
 public:
+    
+    /*
+    내 풀이 방식
     int dfs(TreeNode* cur){
         
         if(cur == NULL)
@@ -46,9 +53,30 @@ public:
 
         return root;
     }
+    */
+    
+    // modification of Inorder Traverse
+    void traverse(TreeNode* root){
+        if (!root) return;
+        
+        traverse(root->right);
+        root->val = (cur_sum += root->val);
+        traverse(root->left);
+    }
+    
+    TreeNode* convertBST(TreeNode* root) {
+        traverse(root);
+        return root;
+    }
 };
 
 /*
-Traverse
+Original Inorder Traverse
+: 작은 수부터 들르면서 자신보다 큰 값들을 찾아서 다 더해주고
+그리고 그것들을 자기보다 작은 수들에게도 동일하게 적용해주기
 
+Modificatoin of Inorder Traverse
+: 큰 수(오른쪽)부터 먼저 들르면서 해당 노드의 값을 sum에 누적시켜놓으면
+어떤 노드에 도착했을 때 sum은 그것보다 큰 수의 누적합으로 저장되어있음
+따라서 그 순서로 traverse만 해주고 도착했을 때 현재 노드에 sum을 더해주기만 하면 됨
 */
