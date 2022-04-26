94c1b2937f225afccd8e7b8686e03c637470029d
MinHK4 <67371123+MinHK4@users.noreply.github.com>
2022-04-26 20:50:36 +0900

    Time: 1448 ms (15.00%), Space: 58.2 MB (57.91%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -0,0 +1,71 @@
+struct edge{
+    int src, dst, cost;
+};
+
+bool sortby(edge a, edge b){
+    return a.cost < b.cost;
+}
+
+class Solution {
+private:
+    vector<int> rank;
+    vector<int> parent;
+
+public:
+    
+    int Find(int cur){
+        if(parent[cur] != cur)
+            parent[cur] = Find(parent[cur]);
+        return parent[cur];
+    }
+    
+    void Union(int a, int b){
+        if(rank[a] < rank[b])
+            parent[a] = b;
+        else if(rank[a] > rank[b])
+            parent[b] = a;
+        else{
+            parent[b] = a;
+            rank[a]++;
+        }
+    }
+    
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        
+        // Graph edge info and sort by cost
+        vector<edge> v;
+        int nums = points.size();
+        for(int i=0; i<nums; i++){
+            for(int j=i+1; j<nums; j++){
+                edge temp;
+                temp.src = i;
+                temp.dst = j;
+                temp.cost = abs(points[i][0]-points[j][0]) + abs(points[i][1] - points[j][1]);
+                v.push_back(temp);
+            }
+        }
+        sort(v.begin(), v.end(), sortby);
+        
+        // Disjoint Set + MST
+        rank.resize(nums, 0);
+        parent.resize(nums);
+        for(int i=0; i<nums; i++)
+            parent[i] = i;
+        
+        int ans = 0;
+        for(edge x : v){
+            int roota = Find(x.src);
+            int rootb = Find(x.dst);
+            if(roota == rootb)
+                continue;            
+            ans += x.cost;
+            Union(roota, rootb);
+        }
+        
+        return ans;
+    }
+};
+
+/*
+MST 풀이 with Disjoint Set
+*/
